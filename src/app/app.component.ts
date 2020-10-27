e2f4a4576675c89e5f984116c0ff880b081bf928
weiz18 <wzhou@yoobic.com>
2020-10-27 19:05:54 +0000

    presence issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,32 +16,33 @@ export class AppComponent {
     this.pubnubInstance = new PubNub({
       publishKey: 'pub-c-186b9547-2104-4b52-a808-01d30d7fa245',
       subscribeKey: 'sub-c-f3c3a1be-5566-11e9-8dfa-9a489f8d8569',
-      logVerbosity: true,
+      // logVerbosity: true,
       uuid: 'myUniqueUUID'
     });
-    const channel = 'test';
 
+
+  }
+
+  initPubnub() {
     this.pubnubInstance.addListener({
       message: (m) => {
         console.log('incoming message', m);
-      }
+      },
+      presence: (p) => {
+        console.log('incoming presence', p);
+      },
     });
+  }
 
-    this.pubnubInstance.subscribe({
-      channels: [channel],
-    });
+  joinChannel(channel) {
+    this.pubnubInstance.subscribe({ channels: [channel], withPresence: true });
+  }
 
-    setTimeout(() => {
-      this.pubnubInstance.publish({ channel, message: { text: ` timestamp: ${new Date()}` } }, (status, response) => {
-        if (status.error) {
-            // handle error
-            console.log(status);
-        } else {
-            console.log('message Published w/ timetoken', response.timetoken);
-            this.pubnubInstance.deleteMessages({channel, start: response.timetoken});
-        }
-    });
-    }, 1000);
+  leaveChannel(channel) {
+    this.pubnubInstance.unsubscribe({ channels: [channel]});
+  }
 
+  resetAll() {
+    this.pubnubInstance.unsubscribeAll();
   }
 }
